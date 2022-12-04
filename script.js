68e0b039da443a1481c008fc330710924fed2402
Sandra  Lima <sandralima.dev@gmail.com>
2022-12-03 22:14:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+
+
+  // e-commerce touchspin
+  $('input[name=\'product-quantity\']').TouchSpin();
+
+(function ($) {
+  'use strict';
+
+  setTimeout(function () {
+    $('.instagram-slider').slick({
+      dots: false,
+      speed: 300,
+      // autoplay: true,
+      arrows: false,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      responsive: [{
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2
+          }
+        }
+      ]
+    });
+  }, 1500);
+
+
+  // e-commerce touchspin
+  $('input[name=\'product-quantity\']').TouchSpin();
+
+
+  $('.hero-slider').slick({
+    // autoplay: true,
+    infinite: true,
+    arrows: true,
+    prevArrow: '<button type=\'button\' class=\'heroSliderArrow prevArrow tf-ion-chevron-left\'></button>',
+    nextArrow: '<button type=\'button\' class=\'heroSliderArrow nextArrow tf-ion-chevron-right\'></button>',
+    dots: true,
+    autoplaySpeed: 7000,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+  $('.hero-slider').slickAnimation();
+
+
+})(jQuery);
+Footer
+
